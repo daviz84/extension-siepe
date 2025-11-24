0e2571d800cf9cb2696c1da6b070e9dd80239371
Davi Pereira de Oliveira <107134223+daviz84@users.noreply.github.com>
2025-11-24 15:39:05 -0300

    Add cpfResp to table row in scriptCadastro.js

--- src/scripts/scriptCadastro.js
+++ src/scripts/scriptCadastro.js
@@ -465,7 +465,7 @@ async function incluiRecursos() {
 
                 if (tabelaDados["RecebeBolsaFamília"] === 'Sim') contadorBolsaFamilia.value = (Number(contadorBolsaFamilia.value) + 1)
 
-                tr.append(matAluno, nameAluno, dataNascimento, naturalidadeAluno, cpfAluno, maeAluno, paiAluno, turmaAluno, nisAluno, tdcodigo_sistema, telefoneResp, rgAluno)
+                tr.append(matAluno, nameAluno, dataNascimento, naturalidadeAluno, cpfAluno, maeAluno, paiAluno, turmaAluno, nisAluno, tdcodigo_sistema, telefoneResp, rgAluno, cpfResp)
 
 
                 if (btncheckOutros.checked == true) {
